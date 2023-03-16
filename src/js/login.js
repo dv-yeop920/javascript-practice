204f3a0691d53ecd5abe7d4eed2e2afb31a4c77c
junyeoplee <jyeop920@gmail.com>
2023-03-16 18:37:30 +0900

    login 간단한 유효성 검사
    모두 입력 안하고 login 버튼 누를시 메세지뜨고 submit 되지 않도록
    그리고 입력 하면 메세지 사라짐
    비밀번호 8자 이상 안될경우 메세지
    8자 이상 되거나 빈 문자열이면 사라짐

---
+++ src/js/login.js
@@ -0,0 +1,52 @@
+const loginForm = document.querySelector(".member");
+const button = document.querySelector(".button");
+const userInputId = document.querySelector("#use-id");
+const userInputPassword = document.querySelector(".userpw");
+const message = document.querySelector(".warning-message");
+const message2 = document.querySelector(".warning-message2");
+const message3 = document.querySelector(".warning-message3");
+const loginButton = document.querySelector("#login-button");
+
+const handleClickButton = () => {
+    button.classList.add("row-button");
+    loginForm.classList.add("show-member");
+}
+
+const handlekeyUpInputId = (event) => {
+    if(event.target.value) {
+        message2.style.display = "none";
+    }
+}
+
+const handlekeyUpInput = (event) => {
+    if(event.target.value){
+        message3.style.display = "none";
+    }
+    if(event.target.value.length < 8) {
+        message.style.display = "block";
+    }else if(event.target.value.length >= 8) {
+        message.style.display = "none";
+    }
+    if(event.target.value.length === 0) {
+        message.style.display = "none";
+    }
+}
+
+const hadleClickSubmit = (event) => {
+    if(userInputId.value === ""){
+        event.preventDefault();
+        message2.style.display = "block";
+    }else if(userInputPassword.value === "") {
+        event.preventDefault();
+        message3.style.display = "block";
+    }
+    if(userInputPassword.value.length < 8) {
+        event.preventDefault();
+    }
+}
+
+
+loginButton.addEventListener("click" , hadleClickSubmit);
+userInputId.addEventListener("keyup" ,handlekeyUpInputId );
+userInputPassword.addEventListener("keyup" , handlekeyUpInput);
+button.addEventListener("click" , handleClickButton);
