0304b9e2a4653aeac81d2290ac2600317fdd3fc4
junyeoplee <jyeop920@gmail.com>
2023-03-22 21:22:24 +0900

    1,2,3 버튼으로 이동하는 slide card 완성
    너무 하드코딩 이지만 일단 완성
    각 버튼을 누르면 classList 를 추가하고 삭제 하는 방식으로 css 애니메이션 적용함

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,2 +1,68 @@
-const passButton = document.querySelector(".pass-button");
+const button = document.querySelectorAll(".button-box");
+const button1 = document.querySelector(".btn-1");
+const button2 = document.querySelector(".btn-2");
+const button3 = document.querySelector(".btn-3");
+const card = document.querySelector(".slide-box");
+
+
+function handleClickButton1() {
+    card.classList.remove("card-slide2");
+    card.classList.remove("card-slide3");
+    card.classList.add("card-slide1");
+}
+
+function handleClickButton2() {
+    card.classList.remove("card-slide1");
+    card.classList.remove("card-slide3");
+    card.classList.add("card-slide2");
+}
+
+function handleClickButton3() {
+    card.classList.remove("card-slide1");
+    card.classList.remove("card-slide2");
+    card.classList.add("card-slide3");
+}
+
+
+
+button1.addEventListener("click" , handleClickButton1);
+button2.addEventListener("click" , handleClickButton2);
+button3.addEventListener("click" , handleClickButton3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const passButton = document.querySelector(".pass-button");
 const backButton = document.querySelector(".back-button");
+
+const slideCard = document.querySelector(".slide-box__card");
+
+
+
+
+
+function handleClickPassButton() {
+    slideCard.classList.add("card-slide__left");
+    slideCard.classList.remove("card-slide__right");
+}
+function handleClickBackButton() {
+    slideCard.classList.remove("card-slide__left");
+    slideCard.classList.add("card-slide__right");
+}
+
+passButton.addEventListener("click" , handleClickPassButton);
+backButton.addEventListener("click" , handleClickBackButton);*/
