8b0d06c70403bb1a3e479effcbb7a755872c79cc
junyeoplee <jyeop920@gmail.com>
2023-03-23 02:41:22 +0900

    화살표 버튼 기능
    화살표 버튼 누를때 앞으로 한칸씩 뒤로 한칸씩 가도록 구현

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,8 +1,12 @@
-const button = document.querySelectorAll(".button-box");
+
 const button1 = document.querySelector(".btn-1");
 const button2 = document.querySelector(".btn-2");
 const button3 = document.querySelector(".btn-3");
+const nextButton = document.querySelector(".next-button");
+const backButton = document.querySelector(".back-button");
+
 const card = document.querySelector(".slide-box");
+
 
 
 function handleClickButton1() {
@@ -24,10 +28,41 @@ function handleClickButton3() {
 }
 
 
+let count = 0;
+let backCount = 1;
+
+
+function handleClickNextButton() {
+    if(count === 0){
+        card.classList.remove("card-slide4");
+        card.classList.add("card-slide2");
+        count++;
+    }else if(count === 1){
+        card.classList.remove("card-slide2");
+        card.classList.add("card-slide3");
+        count--;
+    }
+}
+
+function handleClickBackButton() {
+    if(backCount === 1){
+        card.classList.remove("card-slide3");
+        card.classList.add("card-slide2");
+        backCount --;
+    }else if(backCount === 0){
+        card.classList.remove("card-slide2");
+        card.classList.add("card-slide4");
+        backCount++;
+    }
+}
+
+
 
 button1.addEventListener("click" , handleClickButton1);
 button2.addEventListener("click" , handleClickButton2);
 button3.addEventListener("click" , handleClickButton3);
+nextButton.addEventListener("click" , handleClickNextButton);
+backButton.addEventListener("click" , handleClickBackButton);
 
 
 
