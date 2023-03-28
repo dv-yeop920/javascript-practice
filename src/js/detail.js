e89156be5ef253fb9a3b49cac8c46eb4f2fdb859
junyeoplee <jyeop920@gmail.com>
2023-03-28 23:17:01 +0900

    상품명 , 가격 을 데이터로 뿌려주기 연습
    셀렉터로 title , price 를 전부 불러오고
    반복문을 통해 i 로 title, price 인덱스에 접근해서 순서대로 products 에 있는 데이터를 뿌려 줬다

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,6 +1,22 @@
+/*
+각각 card 에 상품명과 가격을 products 객체에서 데이터를 가져와 각각 다르게 표시해 준다
+*/
 //상품 , 가격 데이터 객체
 const products = [
     { id : 0, price : 70000, title : 'Blossom Dress' },
     { id : 1, price : 50000, title : 'Springfield Shirt' },
     { id : 2, price : 60000, title : 'Black Monastery' }
 ]
+
+const title = document.querySelectorAll(".title");
+const price = document.querySelectorAll(".price");
+
+for(let i = 0; i <= title.length; i++) {
+        title[i].innerHTML = products[i].title;
+        price[i].innerHTML = products[i].price;
+}
+
+
+
+
+
