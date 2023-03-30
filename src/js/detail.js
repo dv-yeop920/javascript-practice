5769fde2ede8cd02dba15d17fb8c90cd96555126
junyeoplee <jyeop920@gmail.com>
2023-03-31 00:02:59 +0900

    sort 로 오름차 내림차 가격순 구현
    나중에 좀더 효율적인 코드 짜보기

--- src/js/detail.js
+++ src/js/detail.js
@@ -10,7 +10,8 @@ const products = [
 
 const title = document.querySelectorAll(".title");
 const price = document.querySelectorAll(".price");
-const priceButton = document.querySelector("#price-button");
+const priceButtonDown = document.querySelector("#price-button");
+const priceButtonUp = document.querySelector("#price-button2");
 
 //기본 정렬 상태 
 for(let i = 0; i < title.length; i++) {
@@ -21,19 +22,31 @@ for(let i = 0; i < title.length; i++) {
 }
 
 //버튼 클릭시 가격순으로 한번 더 정렬
-const handleClickPriceButton = () => {
-    for(let i = 0; i < title.length; i++) {
-        products.sort((a,b)=> {
-            return a.price - b.price;
-        });
-        title[i].innerHTML = products[i].title;
-        price[i].innerHTML = products[i].price;
-        
-}
+const handleClickPriceButtonUpAndDown = (event) => {
+    if(event.target.id === "price-button") {
+            for(let i = 0; i < title.length; i++) {
+                products.sort((a,b)=> {
+                    return a.price - b.price;
+                });
+                title[i].innerHTML = products[i].title;
+                price[i].innerHTML = products[i].price;
+            }
+    }
+    if(event.target.id === "price-button2") {
+        for(let i = 0; i < title.length; i++) {
+            products.sort((a,b)=> {
+                return b.price - a.price;
+            });
+            title[i].innerHTML = products[i].title;
+            price[i].innerHTML = products[i].price;
+        }
+    }
 }
 
-priceButton.addEventListener("click" , handleClickPriceButton);
 
+
+priceButtonDown.addEventListener("click" , handleClickPriceButtonUpAndDown);
+priceButtonUp.addEventListener("click" , handleClickPriceButtonUpAndDown);
 
 
 
