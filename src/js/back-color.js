b4db09b48871c3551426d63155e6501b69a7e1c3
junyeoplee <jyeop920@gmail.com>
2023-04-03 20:07:33 +0900

    .

--- src/js/back-color.js
+++ src/js/back-color.js
@@ -15,12 +15,3 @@ button.addEventListener("click" , ColorClickChange);
 
 
 
-
-const a = 7;
-const b = 3;
-
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(Math.floor(a/b));
-console.log(a%b);
