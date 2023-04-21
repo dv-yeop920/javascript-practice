7ed0edb789a0303d77025a35b378e92aadb31a45
junyeoplee <jyeop920@gmail.com>
2023-04-21 16:41:22 +0900

    animation 추가
    스크롤할때 일정위치 오면 크기가 작아지는 애니메이션 추가

--- src/js/sticky.js
+++ src/js/sticky.js
@@ -2,22 +2,27 @@
 
 function handleScrollWindow() {
     const imgCard = document.querySelectorAll(".img-card");
-    const scroll = window.scrollY
+    //const scroll = window.scrollY;
+    let scroll = window.scrollY;
     console.log(scroll);
 
-    if(window.scrollY >  3580) {
+    if(window.scrollY > 3780) {
         imgCard[0].classList.add("hide");
+        imgCard[0].classList.add("small");
         imgCard[0].classList.remove("show");
     }else {
         imgCard[0].classList.remove("hide");
+        imgCard[0].classList.remove("small");
         imgCard[0].classList.add("show");
     }
     
-    if(window.scrollY >  4380) {
+    if(window.scrollY >  4480) {
         imgCard[1].classList.add("hide");
+        imgCard[1].classList.add("small");
         imgCard[1].classList.remove("show");
     }else {
         imgCard[1].classList.remove("hide");
+        imgCard[1].classList.remove("small");
         imgCard[1].classList.add("show");
     }
 
